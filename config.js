ad4c3a388ac86f1b6b3c1abdfa71ff257fd583dd
willy199710 <finaleternal@gmail.com>
2023-03-15 22:28:21 +0800

    update

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
     app: {
-        token: 'OTY4MjA0OTYzMTY1NjM0NTgz.G2mh7j.MxRyJr58QozDIK2h_UoXrvBcqPZ0bUUSdbuka4',
         playing: '機油',
         global: true,
         guild: 'XXX',
